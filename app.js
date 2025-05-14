11afe89a647175426b371f60b63b8401812d7148
panuwat <panuwat.codes@gmail.com>
2025-05-14 18:08:09 +0700

    refactor: fix problems

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res, _next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -36,6 +36,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+  console.log(user.email); // should trigger warning
 });
 
 module.exports = app;
