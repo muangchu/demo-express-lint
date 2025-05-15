31a3d90fd19c00d964ebd0bae5b6a49596e9fa38
panuwat <panuwat.codes@gmail.com>
2025-05-15 10:43:05 +0700

    check pii logging

--- eslint-rules/no-pii-logging.js
+++ eslint-rules/no-pii-logging.js
@@ -18,22 +18,26 @@ module.exports = {
         CallExpression(node) {
           const isConsoleLog = node.callee.type === "MemberExpression" &&
                                node.callee.object.name === "console" &&
-                               node.callee.property.name === "log";
-  
-          if (isConsoleLog) {
-            node.arguments.forEach(arg => {
-              if (arg.type === "MemberExpression") {
-                const field = arg.property.name || (arg.property.value ?? "");
-                if (piiFields.includes(field)) {
-                  context.report({
-                    node,
-                    messageId: "avoidLoggingPII",
-                    data: { field }
-                  });
-                }
+                               ['log', 'info', 'warn', 'error'].includes(node.callee.property.name);
+          
+          if (!isConsoleLog) return; 
+          
+          for (const arg of node.arguments) {
+            if (arg.type === 'MemberExpression') {
+              const prop = arg.property;
+              const name = prop.name || (prop.value ?? '');
+
+              if (piiFields.includes(name)) {
+                context.report({
+                  node: prop,
+                  messageId: 'avoidLoggingPII',
+                  data: { field: name },
+                });
               }
-            });
+
+            }
           }
+
         }
       };
     }
