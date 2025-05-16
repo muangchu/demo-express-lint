e54e01dae7690a00d890018f680e3760b4030e1c
panuwat <panuwat.codes@gmail.com>
2025-05-16 16:59:52 +0700

    config: add custom rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 
+import noArrayLogging from "./eslint-rules/no-array-logging.js";
 import noBodyJsonLogging from "./eslint-rules/no-body-json-logging.js";
 import noObjectLogging from "./eslint-rules/no-object-logging.js";
 import noPiiLogging from './eslint-rules/no-pii-logging.js'
 import noRequestLogging from "./eslint-rules/no-request-logging.js";
 import noResponseLogging from "./eslint-rules/no-response-logging.js";
+import noSensitiveObjectLogging from "./eslint-rules/no-sensitive-object-logging.js";
 
 export default defineConfig([
 	{ files: ["**/*.js"], languageOptions: { globals: globals.browser } },
@@ -23,20 +25,24 @@ export default defineConfig([
     plugins: {
       local: {
         rules: {
+          'no-array-logging': noArrayLogging,
           'no-body-json-logging': noBodyJsonLogging,
           'no-object-logging': noObjectLogging,
           'no-pii-logging': noPiiLogging,
           'no-request-logging': noRequestLogging,
           'no-response-logging': noResponseLogging,
+          'no-sensitive-object-logging': noSensitiveObjectLogging,
         },
       },
     },
     rules: {
-      'local/no-body-json-logging': 'error',
-      'local/no-object-logging': 'error',
+      'local/no-array-logging': 'off',
+      'local/no-body-json-logging': 'off',
+      'local/no-object-logging': 'off',
       'local/no-pii-logging': 'error',
-      'local/no-request-logging': 'error',
-      'local/no-response-logging': 'error',
+      'local/no-request-logging': 'off',
+      'local/no-response-logging': 'off',
+      'local/no-sensitive-object-logging': 'error',
     },
   },
 ]);
