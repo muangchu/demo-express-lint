b68d375a429c6574079de31db88a8b3fb9ee21c7
panuwat <panuwat.codes@gmail.com>
2025-05-16 16:50:33 +0700

    feat(rule): detect sensitive object logging

---
+++ tests/no-sensitive-object-logging.test.js
@@ -0,0 +1,10 @@
+logger.debug(payload);
+this.logger.info(meta);
+console.log(token);
+log.info(header);
+
+logger.info(payload);
+this.logger.debug(meta);
+console.log({ token });
+log.warn({ header: headers });
+this.logger.debug(appMeta);
